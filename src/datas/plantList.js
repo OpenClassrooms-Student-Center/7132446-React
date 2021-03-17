7e707a9e20217e3c635c461ecd615ed93623e6ae
Max Mahmoud Wardeh <m@lurnid.com>
2021-03-17 17:43:02 +0000

    Update translations

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -3,7 +3,7 @@ import monstera from '../assets/monstera.jpg'
 export const plantList = [
 	{
 		name: 'monstera',
-		category: 'classique',
+		category: 'classic',
 		id: '1ed',
 		light: 2,
 		water: 3,
@@ -11,15 +11,15 @@ export const plantList = [
 	},
 	{
 		name: 'ficus lyrata',
-		category: 'classique',
+		category: 'classic',
 		id: '2ab',
 		light: 3,
 		water: 1,
 		cover: monstera
 	},
 	{
-		name: 'pothos argenté',
-		category: 'classique',
+		name: 'silver pothos',
+		category: 'classic',
 		id: '3sd',
 		light: 1,
 		water: 2,
@@ -27,7 +27,7 @@ export const plantList = [
 	},
 	{
 		name: 'yucca',
-		category: 'classique',
+		category: 'classic',
 		id: '4kk',
 		light: 3,
 		water: 1,
@@ -35,23 +35,23 @@ export const plantList = [
 	},
 	{
 		name: 'olivier',
-		category: 'extérieur',
+		category: 'exterior',
 		id: '5pl',
 		light: 3,
 		water: 1,
 		cover: monstera
 	},
 	{
-		name: 'géranium',
-		category: 'extérieur',
+		name: 'geranium',
+		category: 'exterior',
 		id: '6uo',
 		light: 2,
 		water: 2,
 		cover: monstera
 	},
 	{
-		name: 'basilique',
-		category: 'extérieur',
+		name: 'basilica',
+		category: 'exterior',
 		id: '7ie',
 		light: 2,
 		water: 3,
@@ -59,15 +59,15 @@ export const plantList = [
 	},
 	{
 		name: 'aloe',
-		category: 'plante grasse',
+		category: 'oil plant',
 		id: '8fp',
 		light: 2,
 		water: 1,
 		cover: monstera
 	},
 	{
-		name: 'succulente',
-		category: 'plante grasse',
+		name: 'succulent',
+		category: 'oil plant',
 		id: '9vn',
 		light: 2,
 		water: 1,
