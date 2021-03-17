7c7fe86688b0233cd541bd9b03d4521b6fed259c
Max Mahmoud Wardeh <m@lurnid.com>
2021-03-17 16:17:31 +0000

    Update translations

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,10 @@ import logo from '../assets/logo.png'
 import '../styles/Banner.css'
 
 function Banner() {
-	const title = 'La maison jungle'
+	const title = 'Jungle House'
 	return (
 		<div className='lmj-banner'>
-			<img src={logo} alt='La maisonjungle' className='lmj-logo' />
+			<img src={logo} alt='Jungle House' className='lmj-logo' />
 			<h1 className='lmj-title'>{title}</h1>
 		</div>
 	)
