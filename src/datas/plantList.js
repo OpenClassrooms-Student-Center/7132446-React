dc31d81794ed2d24b5996c9976987d26f2c88905
Max Mahmoud Wardeh <m@lurnid.com>
2021-04-08 16:25:17 +0100

    Update translation

--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -21,7 +21,7 @@ export const plantList = [
 		id: '4kk'
 	},
 	{
-		name: 'olivier',
+		name: 'olive tree',
 		category: 'exterior',
 		id: '5pl'
 	},
@@ -31,7 +31,7 @@ export const plantList = [
 		id: '6uo'
 	},
 	{
-		name: 'basilica',
+		name: 'basil',
 		category: 'exterior',
 		id: '7ie',
 		isSpecialOffer: true
