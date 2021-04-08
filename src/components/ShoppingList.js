9a22512b08b896d9783cf527d590459b4f415e74
Max Mahmoud Wardeh <m@lurnid.com>
2021-04-08 09:46:30 +0100

    Replace lmj-* class names with jh-*

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -16,7 +16,7 @@ function ShoppingList({ cart, updateCart }) {
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul className='lmj-plant-list'>
+			<ul className='jh-plant-list'>
 				{plantList.map(({ id, cover, name, water, light }) => (
 					<div key={id}>
 						<PlantItem cover={cover} name={name} water={water} light={light} />
