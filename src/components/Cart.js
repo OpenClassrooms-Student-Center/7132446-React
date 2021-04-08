f9d879b87f057fe044c47363d54b29f70185e381
Max Mahmoud Wardeh <m@lurnid.com>
2021-04-08 09:45:38 +0100

    Replace lmj-* class names with jh-*

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,7 +5,7 @@ function Cart() {
 	const ivyPrice = 10
 	const flowerPrice = 15
 	return (
-		<div className='lmj-cart'>
+		<div className='jh-cart'>
 			<h2>Panier</h2>
 			<ul>
 				<li>Monstera : {monsteraPrice}€</li>
